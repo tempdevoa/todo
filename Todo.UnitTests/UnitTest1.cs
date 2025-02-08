71587df767537a63c0dfb97ba9fc6ee1fa30203e
Andre Orlowski <andre.orlowski@gmx.de>
2025-02-08 07:51:44 +0100

    Added UnitTests project

---
+++ Todo.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Todo.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
